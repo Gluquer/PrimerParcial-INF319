bda9ed22622aede1da24a7a47f14e4da6605f52a
Gluquer <38804639+Gluquer@users.noreply.github.com>
2021-09-22 23:22:23 -0400

    Add files via upload

---
+++ Pregunta1/Pregunta1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pregunta1
+{
+    public partial class Form1 : Form
+    {
+        int valor;
+        int valor1;
+        int resultado;
+        int sws = 0, swr=0, swm =0,swd=0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            valor = 0;
+            valor1 = 0;
+            resultado = 0;
+            sws = swr = swm = swd = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text != ("+") && ((Button)sender).Text != ("-") && ((Button)sender).Text != ("*")
+                && ((Button)sender).Text != ("/") && ((Button)sender).Text != ("C") && ((Button)sender).Text != ("="))
+            {
+                valor = valor * 10 + Convert.ToInt32(((Button)sender).Text);
+                textBox1.Text = valor.ToString();
+            }
+            else
+            {
+                if (((Button)sender).Text.Equals("="))
+                {
+                    if (sws == 1)
+                    {
+                        resultado = valor1 + valor;
+                    }
+                    if (swr == 1)
+                    {
+                        resultado = valor1 - valor;
+                    }
+                    if (swm == 1)
+                    {
+                        resultado = valor1 * valor;
+                    }
+                    if (swd == 1)
+                    {
+                        if (valor != 0) { 
+                            resultado = valor1 / valor;
+                        }
+                    }
+                    textBox1.Text = resultado.ToString();
+                    resultado = 0;
+                    sws = swr = swm = swd = 0;
+                }
+                else
+                {
+                    valor1 = valor;
+                    if (((Button)sender).Text.Equals("+"))
+                    {
+
+                        textBox1.Text = "";
+                        valor1 = valor;
+                        valor = 0;
+                        sws = 1;
+                    }
+                    if (((Button)sender).Text.Equals("-"))
+                    {
+                        textBox1.Text = "";
+                        valor = 0;
+                        swr = 1;
+                    }
+                    if (((Button)sender).Text.Equals("*"))
+                    {
+                        textBox1.Text = "";
+                        valor = 0;
+                        swm = 1;
+                    }
+                    if (((Button)sender).Text.Equals("/"))
+                    {
+                        textBox1.Text = "";
+                        valor = 0;
+                        swd = 1;
+                    }
+                    if (((Button)sender).Text.Equals("C"))
+                    {
+                        valor = 0;
+                        textBox1.Text = "";
+                        valor1 = 0;
+                        resultado = 0;
+                        sws = swr = swm = swd = 0;
+                    }
+                }
+            }            
+    }
+    }
+}
