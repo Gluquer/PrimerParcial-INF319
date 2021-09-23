afc47e93a24f026d2263acefb7df8b4c3a22571e
Gluquer <38804639+Gluquer@users.noreply.github.com>
2021-09-22 23:09:48 -0400

    Add files via upload

---
+++ pregunta5/pregunta5/Program.fs
@@ -0,0 +1,15 @@
+﻿// Más información acerca de F# en http://fsharp.org
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+ 
+[<EntryPoint>]
+let main argv = 
+    
+    let n=6
+    let m=2
+    let mutable r = 0
+    for i = 1 to n do
+       r <- r + m
+    printfn "Resultado :%i " r
+    Console.ReadKey() |>ignore
+    0 // devolver un código de salida entero
